140d99393e1c426671f383ce0e3e6e223bcb2b11
David <bluegamingxp@gmail.com>
2025-04-04 19:01:42 -0400

    Module 5 completed baseline

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,10 +1,35 @@
-var fs = require('fs');
-var trips = JSON.parse(fs.readFileSync("./data/trips.json","utf8"));
+const tripEndpoint = 'http://localhost:3000/api/trips';
+const options = {
+    method: 'GET',
+    headers: {
+        'Accept': 'application/json'
+    }
+}
 
-const travel = (req, res) => {
-    pageTitle = process.env.npm_package_description + ' - Travel';
-    res.render('travel', {title: pageTitle, trips});
-};
+// var fs = require('fs');
+// var trips = JSON.parse(fs.readFileSync("./data/trips.json","utf8"));
+
+// const travel = (req, res) => {
+//     res.render('travel', {title: 'Travlr Gateways - Travel', trips});
+// };
+
+const travel = async function(req, res, next) {
+    await fetch(tripEndpoint, options)
+        .then(res => res.json())
+        .then(json => {
+            let message = null;
+            if(!json instanceof Array) {
+                message = 'API lookup error'
+                json = [];
+            } else {
+                if(!json.length) {
+                    message = 'No trips exist in the database'
+                }
+            }
+            res.render('travel', {title: 'Travlr Gateways - Travel', trips: json, message});
+        })
+        .catch(err => res.status(500).send(e.message))
+}
 
 module.exports = {
     travel
